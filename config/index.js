3af1ef9d0b9c7b5020a3b636590ad02b6e63db84
baipiaoguaiisme <105282871+baipiaoguaiisme@users.noreply.github.com>
2022-09-03 11:35:58 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -17,7 +17,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "o4sTH6ImylubBREB-s8LYz963Rg8",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "ycHULnFxd46TTIqPKiReYzs_oWZeOydB5RMtg4tGy3Y",
+      useTemplateId: "	0tDCkA5-_RPLxK8AMZO63ALx-inYPP26NnLJKtuNODc",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: ""
     }
@@ -29,7 +29,7 @@ export const config = {
    */
 
   // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "ycHULnFxd46TTIqPKiReYzs_oWZeOydB5RMtg4tGy3Y",
+  CALLBACK_TEMPLATE_ID: "	0tDCkA5-_RPLxK8AMZO63ALx-inYPP26NnLJKtuNODc",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
