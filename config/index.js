dfbd0596f3ce632b62a7bef2ba2c47a3df66050d
baipiaoguaiisme <105282871+baipiaoguaiisme@users.noreply.github.com>
2022-09-03 12:14:35 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -17,7 +17,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "o4sTH6ImylubBREB-s8LYz963Rg8",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "0tDCkA5-_RPLxK8AMZO63ALx-inYPP26NnLJKtuNODc",
+      useTemplateId: "Ii_kV_XKUhEWVhgbFezqqGyDgSvlnZZi5DEcRJngA2o",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: ""
     },
@@ -27,7 +27,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "o4sTH6LSjAiYQm25NYyI438yVIYQ",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "0tDCkA5-_RPLxK8AMZO63ALx-inYPP26NnLJKtuNODc",
+      useTemplateId: "Ii_kV_XKUhEWVhgbFezqqGyDgSvlnZZi5DEcRJngA2o",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: ""}
 
@@ -38,7 +38,7 @@ export const config = {
    */
 
   // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "0tDCkA5-_RPLxK8AMZO63ALx-inYPP26NnLJKtuNODc",
+  CALLBACK_TEMPLATE_ID: "Ii_kV_XKUhEWVhgbFezqqGyDgSvlnZZi5DEcRJngA2o",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
@@ -59,7 +59,7 @@ export const config = {
   // 所在省份
   PROVINCE: "湖南",
   // 所在城市
-  CITY: "永州",
+  CITY: "邵阳",
 
   /** 重要节日相关 */
 
